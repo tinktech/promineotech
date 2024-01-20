898a4095f96124031a75d101971dc3e63e4f97d2
tdayetech <150866869+tdayetech@users.noreply.github.com>
2024-01-20 14:30:43 -0600

    week8 finished

--- Week8/index.js
+++ Week8/index.js
@@ -6,7 +6,7 @@ class Pet {
         this.name = name;
         this.type = type;
         this.color = color;
-    }
+    } // pet class and variables
 
     describe() {
         return `${this.name}, a ${this.color} ${this.type}`;
@@ -15,8 +15,8 @@ class Pet {
 class Owner {
     constructor(name) {
         this.name = name;
-        this.pets = [];
-    }
+        this.pets = []; // empty pets array
+    } // owner class and variable
 
     addPet(pet) {
         if (pet instanceof Pet) {
@@ -24,7 +24,7 @@ class Owner {
         } else {
             throw new Error(`You can only add an instance of Pet. Argument is not a pet: ${pet}`);
         }
-    }
+    } // method for adding a pet to an owner
 
     describe() {
         return `${this.name} has ${this.pets.length} pets.`;
@@ -33,7 +33,7 @@ class Owner {
 
 class Menu {
     constructor() {
-        this.owners = [];
+        this.owners = []; // empty owners array
         this.selectedOwner = null;
     }
 
@@ -61,7 +61,7 @@ class Menu {
             selection = this.showMainMenuOptions();
         }
         alert('Goodbye!');
-    }
+    } // menu functionality; choice selection
 
 
     showMainMenuOptions() {
@@ -92,25 +92,25 @@ ${ownerInfo}
                 }
             } else {
                 ownerString = `There are currently no pet owners.`;
-            }
+            } // if else for if there are any owners, and to display them when there are
         alert(ownerString);
-    }
+    } // displays all owners
 
     createOwner() {
         let name = prompt('Enter name for a new pet owner: ');
         this.owners.push(new Owner(name));
-    }
+    } // creates a new owner
 
     viewOwner() {
         let index = prompt("Enter the index of the pet owner you wish to view:");
         if (index > -1 && index < this.owners.length) {
             this.selectedOwner = this.owners[index];
-            let description = 'Owner Name: ' + this.selectedOwner.name + '\n';
-            description += '' + this.selectedOwner.describe() + '\n';
+            let description = 'Owner Name: ' + this.selectedOwner.name + '\n'; // displays selected owner's name
+            description += '' + this.selectedOwner.describe() + '\n'; // displays selected owner's description(number of pets)
 
             for (let i = 0; i < this.selectedOwner.pets.length; i++) {
                 description += i + ') ' + this.selectedOwner.pets[i].describe() + '\n';
-            }
+            } // displays current owner's pets
 
             let selection1 = this.showOwnerMenuOptions(description);
             switch (selection1) {
@@ -119,7 +119,7 @@ ${ownerInfo}
                 break;
                 case '2' :
                 this.deletePet();
-            }
+            } // adding/deleting pets functionality
         }
     }
 
@@ -128,7 +128,7 @@ ${ownerInfo}
         if (index > -1 && index < this.owners.length) {
             this.owners.splice(index,1);
         }
-    }
+    } // removes selected owner
 
 
     createPet() {
@@ -136,14 +136,15 @@ ${ownerInfo}
         let type = prompt('Enter the type of new pet(cat/dog/etc): ');
         let color = prompt('Enter the color of new pet: ');
         this.selectedOwner.addPet(new Pet(name,type,color));
-    }
+    } // creates a new pet
 
     deletePet() {
         let index = prompt('Enter the index of the pet you wish to delete: ');
         if (index > -1 && index < this.selectedOwner.pets.length) {
             this.selectedOwner.pets.splice(index,1);
         }
-    }
+    } // deletes selected pet
 }
+
 let menu = new Menu();
-menu.start();
+menu.start(); // launch menu.start()
